8799e7c01656df425f12c7df876b898d2e14798e
vikashsah1309 <vkumarsah13@gmail.com>
2018-12-25 23:21:33 +0530

    SETNX and GETSET key have done.

--- RedisCache/Program.cs
+++ RedisCache/Program.cs
@@ -22,7 +22,13 @@ namespace RedisCache
             Console.WriteLine(IsKeyDelete(key));
             Console.ReadLine();
         }
-
+        
+        /// <summary>
+        /// SET key “value”: This key sets a value for a key O(1)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="data"></param>
         public static void SetData<T>(string key, T data)
         {
             using (var redis = ConnectionMultiplexer.Connect(RedisConnetion))
@@ -36,6 +42,13 @@ namespace RedisCache
                 redis.Close();
             }
         }
+       
+        /// <summary>
+        /// GET key: This key gets the value O(1)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static T GetData<T>(string key)
         {
             using (var redis = ConnectionMultiplexer.Connect(RedisConnetion))
@@ -58,6 +71,63 @@ namespace RedisCache
 
             }
         }
+        
+        /// <summary>
+        /// GETSET key “value”: Get the old value and sets a new value O(1)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool GetSetData<T>(string key, T data)
+        {
+            bool Isexits = false;
+            using (var redis = ConnectionMultiplexer.Connect(RedisConnetion))
+            {
+                IDatabase db = redis.GetDatabase();
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                json_serializer.MaxJsonLength = int.MaxValue;
+                db.StringGetSet(key, json_serializer.Serialize(data));
+
+                redis.Close();
+            }
+            return Isexits;
+        }
+       
+        /// <summary>
+        /// SETNX key “value”: If key does Not eXist this will sets a value against it O(1)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool SETNXData<T>(string key, T data)
+        {
+            bool Isexits = false;
+            using (var redis = ConnectionMultiplexer.Connect(RedisConnetion))
+            {
+                IDatabase db = redis.GetDatabase();
+                JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+                json_serializer.MaxJsonLength = int.MaxValue;
+
+                if (db.StringSet(key, json_serializer.Serialize(data), TimeSpan.MaxValue, When.NotExists))
+                {
+                    var val = db.StringGet(key);
+                    Console.WriteLine("StringGet({0}) value is {1}", key, val);
+                }
+                else
+                {
+                    //always goes here
+                    Console.WriteLine("Value already exist");
+                }
+                redis.Close();
+            }
+            return Isexits;
+        }
+       
+        /// <summary>
+        /// KeyExists : Check key exit or not.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static bool IsKeyExits(string key)
         {
             bool Isexits = false;
@@ -70,6 +140,12 @@ namespace RedisCache
             }
             return Isexits;
         }
+        
+        /// <summary>
+        /// KeyDelete :  exited key delete from redis.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static bool IsKeyDelete(string key)
         {
             bool Isexits = false;
@@ -82,5 +158,6 @@ namespace RedisCache
             }
             return Isexits;
         }
+        
     }
 }
